f2bec8cdccd8d89f53e3c5b8002cb9722fb7d41a
Dmitry Sedykh <sedykh@gmail.com>
2019-12-24 17:01:29 +0300

    example

--- example_test.go
+++ example_test.go
@@ -1,15 +1,14 @@
-package attributes_test
+package attributes
 
 import (
 	"log"
 	"os"
 
-	attributes "github.com/mdigger/goldmark-attributes"
 	"github.com/yuin/goldmark"
 )
 
 func Example() {
-	var md = goldmark.New(attributes.Enable)
+	var md = goldmark.New(Enable)
 	var source = []byte("{#id .class1}\ntext")
 	err := md.Convert(source, os.Stdout)
 	if err != nil {
